bc04301115858c9a04a45f1c298f814fb0f4a109
Celine Valentine <cyv419@gmail.com>
2020-04-16 14:40:19 -0700

    all tests passed

--- helpers.test.js
+++ helpers.test.js
@@ -1,40 +1,62 @@
 describe("helpers test with set up and tear down", function() {
 
-            beforeEach(function() {
-                billAmtInput.value = "$100";
-                tipAmtInput.value = "20";
+    beforeEach(function() {
+        billAmtInput.value = 100;
+        tipAmtInput.value = 20;
+        submitPaymentInfo();
 
-            });
+    });
 
-            it("should test sumPaymentTotal()", function() {
-                sumPaymentTotal(type);
-                expect(allPayments["billAmt"]).toEqual("$100");
-                expect(allPayments["tipAmt"]).toEqual("$20");
+    it("should test total of tip amount on sumPaymentTotal()", function() {
+        expect(sumPaymentTotal("tipAmt")).toEqual(20);
 
+        billAmtInput.value = 120;
+        tipAmtInput.value = 20;
+        submitPaymentInfo();
 
+        expect(sumPaymentTotal("tipAmt")).toEqual(40);
 
+    });
+    it("should test total of bill amount on sumPaymentTotal()", function() {
+        expect(sumPaymentTotal("billAmt")).toEqual(100);
 
+        billAmtInput.value = 120;
+        tipAmtInput.value = 20;
+        submitPaymentInfo();
+        expect(sumPaymentTotal("billAmt")).toEqual(220);
 
-            });
-            it("should test calculateTipPercent()", function() {
+    });
+    it("should test calculateTipPercent()", function() {
 
-                calculateTipPercent();
+        expect(calculateTipPercent(100, 20)).toEqual(20);
+        expect(calculateTipPercent(180, 20)).toEqual(11);
+    });
+    it("should test appendTd(tr, value)", function() {
+        let newTr = document.createElement("tr");
+        appendTd(newTr, "test");
+        expect(newTr.children.length).toEqual(1);
+        expect(newTr.firstChild.innerHTML).toEqual("test");
+    });
+    it("should delete button and click handler on appendDeleteBtn(tr, type)", function() {
+        let newTr = document.createElement("tr");
 
+        appendDeleteBtn(newTr);
 
+        expect(newTr.children.length).toEqual(1);
+        expect(newTr.firstChild.innerHTML).toEqual("X");
 
-            });
-            it("should test appendTD()", function() {
-                appendTd();
+    });
 
+    afterEach(function() {
 
-
-
-            });
-
-            afterEach(function() {
-
-
-
-
-
-            });
+        allPayments = {};
+        paymentId = 0;
+        billAmtInput.value = "";
+        tipAmtInput.value = "";
+        serverTbody.innerHTML = "";
+        summaryTds[0].HTML = "";
+        summaryTds[1].HTML = "";
+        summaryTds[2].HTML = "";
+        paymentTbody.innerHTML = "";
+    });
+});
--- servers.test.js
+++ servers.test.js
@@ -1,17 +1,41 @@
 describe("Servers test (with setup and tear-down)", function() {
-  beforeEach(function () {
-    // initialization logic
-    serverNameInput.value = 'Alice';
-  });
+    beforeEach(function() {
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
-    submitServerInfo();
+        serverNameInput.value = "Alice";
+    });
 
-    expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
-  });
+    it("should add a new server to allServers on submitServerInfo()", function() {
 
-  afterEach(function() {
-    // teardown logic
-  });
+        submitServerInfo();
+
+        expect(Object.keys(allServers).length).toEqual(1);
+        expect(allServers['server' + serverId].serverName).toEqual("Alice");
+    });
+    it("should not a new server to allServers on submitServerInfo() with an empty string", function() {
+        serverNameInput.value = "";
+        submitServerInfo();
+
+        expect(Object.keys(allServers).length).toEqual(0);
+
+    });
+    it("should create table to updateServerTable()", function() {
+        submitServerInfo();
+        updateServerTable();
+
+        let currentTd = document.querySelectorAll("#serverTable tbody tr td")
+
+        expect(currentTd.length).toEqual(3);
+        expect(currentTd[0].innerText).toEqual("Alice");
+        expect(currentTd[1].innerText).toEqual("$0.00");
+        expect(currentTd[2].innerText).toEqual("X");
+
+    });
+
+    afterEach(function() {
+        // teardown logic
+        allServers = {};
+        serverId = 0;
+        serverTbody.innerHTML = "";
+
+    });
 });
